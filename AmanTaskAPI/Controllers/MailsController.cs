f0f897262c356b55238bbbc603822ab34c93954c
mohammed.gmahmoud <mohammed.gmahmoud@AFS-TECH-118977>
2024-12-04 07:03:02 +0200

    test2

--- AmanTaskAPI/Controllers/MailsController.cs
+++ AmanTaskAPI/Controllers/MailsController.cs
@@ -23,7 +23,7 @@ namespace AmanTaskAPI.Controllers
         {
             _context = context;
             _receiverRepository = receiverRepository;
-            _messageRepository = messageRepository; k
+            _messageRepository = messageRepository; m
         }
         #endregion
 
